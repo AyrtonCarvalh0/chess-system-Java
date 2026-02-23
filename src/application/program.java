1a134a7c7e3c5f404bcbab20d5a8e1ce12893b34
Ayrton Carvalho <cayrton021@gmail.com>
2026-02-23 18:24:22 -0300

    Projected created

---
+++ src/application/program.java
@@ -0,0 +1,9 @@
+package application;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+    }
+}
