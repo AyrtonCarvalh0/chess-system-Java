e8ebe82ee5260727c450c1b27af9cd1a2d23e0c7
Ayrton Carvalho <cayrton021@gmail.com>
2026-02-23 18:46:39 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+
+    private int rows;
+    private int columns;
+
+    public Position(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    @Override
+    public String toString() {
+        return rows + "," +columns ;
+    }
+}
